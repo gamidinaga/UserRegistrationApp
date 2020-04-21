f156c53c17693364aca630470578e40ed77e318d
gamidinaga <nagarajugamidi138@gmail.com>
2020-04-21 12:36:18 +0530

    some modified

--- StudentProject/src/main/java/com/restfull/util/DBConection.java
+++ StudentProject/src/main/java/com/restfull/util/DBConection.java
@@ -1,5 +1,39 @@
 package com.restfull.util;
 
-public class DBConection {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.ResourceBundle;
 
+public class DBConection {
+	private static final DBConection dbConection = null;
+	static String DB_URL;
+	static String DB_USER;
+	static String DB_PASS;
+    static {
+    	ResourceBundle rb=ResourceBundle.getBundle("db.properties");
+    	DB_URL=rb.getString("db.url");
+    	DB_USER=rb.getString("db.username");
+    	DB_PASS=rb.getString("db.password");
+    }
+	private void creatConnection() {
+		Connection con=null;
+		
+		try {
+			con=DriverManager.getConnection(DB_URL,DB_USER,DB_PASS);
+			
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+	
+	
+	// factory method to get this class object as singleton
+	public static DBConection getConnObj() {
+		if(dbConection!=null) {
+			return dbConection;
+		}
+		return new DBConection();
+	}
 }
